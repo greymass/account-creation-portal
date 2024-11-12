f1c8513e5b34014ad512117ad5fe84a96e118d27
dafuga <danielfugere28@gmail.com>
2024-11-12 10:59:54 -0800

    cleanup: more cleanup

--- src/routes/api/ticket/[ticket]/+server.ts
+++ src/routes/api/ticket/[ticket]/+server.ts
@@ -2,11 +2,10 @@ import { json, type RequestHandler } from '@sveltejs/kit'
 import { SextantError, verifyTicket } from '$lib/sextant'
 
 export const GET: RequestHandler = async ({ params }) => {
-    try {        
+    try {
         if (!params.ticket) {
             return json({ error: 'Missing creation code' }, { status: 400 })
         }
-        
         const result = await verifyTicket(params.ticket)
 
         return json(result, { status: 200 })
